abc016d35357ba243c3c4f65e7990f504964ab5f
SamuelGuibor <guiborsamuel@gmail.com>
2025-07-26 15:57:16 -0300

    dale

--- app/_components/transparencia.tsx
+++ app/_components/transparencia.tsx
@@ -48,29 +48,35 @@ export default function TransparenciaAdmin() {
   const institucionalBlocks = [
     {
       id: 1,
-      title: 'Estatuto Social',
-      url: '#',
+      title: 'Modelo Parecer Fiscal (2022)',
+      url: 'https://drive.google.com/file/d/1HsRo00oqQ-eJj-i5NR8ZqJuouVGgf13-/view',
     },
     {
       id: 2,
-      title: 'Regimento Interno',
-      url: '#',
+      title: 'Modelo Parecer Fiscal (2023)',
+      url: 'https://drive.google.com/file/d/1YRo9gDvW7qgg0guJ8NFw4swwAlfhu6pb/view',
     },
     {
       id: 3,
-      title: 'Relatório Anual Institucional 2022',
-      url: '#',
+      title: 'Ata Homologação Diretoria (2024-2026)',
+      url: 'https://drive.google.com/file/d/1be5AbvFbQh-cgof2-m23rNRdSuJu6LZh/view',
     },
     {
       id: 4,
-      title: 'Plano Estratégico 2023-2025',
-      url: '#',
+      title: 'Estatuto',
+      url: 'https://drive.google.com/file/d/1ezlGXPpgeCZZ_NmwAHpv7H8V5jAynTNa/view',
     },
     {
       id: 5,
-      title: 'Plano Estratégico 2020-2022',
-      url: '#',
+      title: 'Analise Frequencia (2 Semestre de 2023 ao 1 Semestre 2025)',
+      url: 'https://drive.google.com/file/d/1yfvfsxEx9YfWRtFWDEDiBi2x06Z6-oAK/view',
     },
+    {
+      id: 6,
+      title: 'Edital Para Eleição',
+      url: 'https://drive.google.com/file/d/1BLgYbRKIc7gIc27i1rIUqvYwIy25mlvm/views',
+    },
+    
   ];
 
   const projetosIncentivadosBlocks = [
@@ -89,6 +95,20 @@ export default function TransparenciaAdmin() {
       title: 'Iniciativa de Educação 2022',
       url: '#',
     },
+  ];
+
+    const uniformesBlocks = [
+    {
+      id: 1,
+      title: 'Uniforme Branco',
+      url: 'https://drive.google.com/file/d/1mN_DNS01QSI-oJGOmuFgljufhRO8EAdF/view',
+    },
+    {
+      id: 2,
+      title: 'Uniforme Preto',
+      url: 'https://drive.google.com/file/d/1OcuYfMeCh_oeY9HVaguaWj7WFeQj8eOv/view',
+    },
+
   ];
 
   useEffect(() => {
@@ -126,11 +146,12 @@ export default function TransparenciaAdmin() {
 
     <div className="p-4 max-w-7xl mx-auto pt-24">
         <header id="transparencia-header" className="flex flex-wrap justify-center gap-6 mb-12">
-        <a href="#prestacao-de-contas" className="text-blue-800 font-semibold hover:underline transition">Prestação de Contas</a>
-        <a href="#certificacoes" className="text-blue-800 font-semibold hover:underline transition">Certificações</a>
-        <a href="#institucional" className="text-blue-800 font-semibold hover:underline transition">Institucional</a>
-        <a href="#projetos-incentivados" className="text-blue-800 font-semibold hover:underline transition">Projetos Incentivados</a>
-      </header>
+          <a href="#prestacao-de-contas" className="text-blue-800 font-semibold hover:underline transition">Prestação de Contas</a>
+          <a href="#certificacoes" className="text-blue-800 font-semibold hover:underline transition">Certificações</a>
+          <a href="#uniformes" className="text-blue-800 font-semibold hover:underline transition">Uniformes</a>
+          <a href="#institucional" className="text-blue-800 font-semibold hover:underline transition">Institucional</a>
+          <a href="#projetos-incentivados" className="text-blue-800 font-semibold hover:underline transition">Projetos Incentivados</a>
+        </header>
       <section id="prestacao-de-contas">
       <h2 className="text-2xl font-bold text-blue-800 mb-6 text-center">PRESTAÇÃO DE CONTAS</h2>
       <div className="flex flex-wrap justify-center gap-32 mb-6 pb-10">
@@ -202,6 +223,44 @@ export default function TransparenciaAdmin() {
               Clique aqui e baixe o arquivo
             </a>
             {index < certificacoesBlocks.length - 1 && (
+              <div className="w-10 h-0.5 bg-yellow-400 mx-auto mt-2"></div>
+            )}
+          </div>
+        ))}
+      </div>
+      </section>
+
+      <section id="uniformes">
+      <h2 className="text-2xl font-bold text-blue-800 mb-6 text-center">Uniformes</h2>
+      <div className="flex flex-wrap justify-center gap-8 mb-6 pb-10">
+        {uniformesBlocks.map((block, index) => (
+          <div key={block.id} className="flex flex-col items-center w-48">
+            <div className="bg-yellow-400 rounded-full p-2 mb-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 text-white"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 16v2a2 2 0 002 2h12a2 2 0 002-2v-2M7 10l5 5 5-5M12 4v12"
+                />
+              </svg>
+            </div>
+            <p className="font-bold text-blue-800 mb-1 text-center">{block.title}</p>
+            <a
+              href={block.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-500 hover:underline text-center"
+            >
+              Clique aqui e baixe o arquivo
+            </a>
+            {index < uniformesBlocks.length - 1 && (
               <div className="w-10 h-0.5 bg-yellow-400 mx-auto mt-2"></div>
             )}
           </div>
